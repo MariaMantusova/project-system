ca515d54b6dfc886d40974930ad40ddd11fe08ef
maria <maria.mantusova@mail.ru>
2025-05-28 23:19:13 +0400

    create projoject, add ESlint, prettier, react-router

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  { ignores: ['eslint.config.mjs', 'build/**', 'dist/**'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]);
