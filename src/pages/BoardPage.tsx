a3a77620aac4c0436606eb8b68b62496ded44846
maria <maria.mantusova@mail.ru>
2025-05-29 16:34:01 +0400

    feat: add form component

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import MainForm from '../components/MainForm/MainForm';
 
 function BoardPage() {
   const { id } = useParams();
 
-  return <p> Это Board Page № {id} </p>;
+  return (
+    <>
+      <p> Это Board Page № {id} </p>
+      <MainForm />
+    </>
+  );
 }
 
 export default BoardPage;
